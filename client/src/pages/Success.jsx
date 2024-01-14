94c51837f05bd68448d8f3ee98ddd8726b50ef0c
alineking5388 <alineking5388@gmail.com>
2024-01-14 13:09:31 +0530

    success page add

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { FaThumbsUp } from "react-icons/fa";
 
 const Success = () => {
   return (
-    <div>Success</div>
+    <div className="container flex items-center justify-center">
+    <div className='flex relative m-10 flex-col rounded-full bg-blue-200 w-60 h-60 items-center justify-center '>
+        <FaThumbsUp className='text-4xl text-green-600' />
+      <h1 className='font-bold text-3xl text-green-500'>Success!</h1>
+      <p className='text-slate-600 font-bold text-md'>You Are Apply Successfully!</p>
+    </div>
+    
+  </div>
   )
 }
 
