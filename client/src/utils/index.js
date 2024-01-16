fe1988d181a8a301981dd2566a6afa683704a69d
alineking5388 <alineking5388@gmail.com>
2024-01-16 23:17:02 +0530

    cheng origin

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000/api-v1";
-// const API_URL = "https://job-finder-snqi.onrender.com/api-v1";
+// const API_URL = "http://localhost:5000/api-v1";
+const API_URL = "https://job-finder-snqi.onrender.com/api-v1";
 
 export const API =axios.create({
     baseURL: API_URL,
