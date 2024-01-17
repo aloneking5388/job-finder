7e3d471f4168203b6f454f95e8fe3214cc0a0545
alineking5388 <alineking5388@gmail.com>
2024-01-17 10:37:14 +0530

    check out page create

--- client/src/components/CheckOutForm.jsx
+++ client/src/components/CheckOutForm.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 import axios from 'axios';
@@ -20,13 +19,13 @@ const CheckOutForm = () => {
   };
 
   return (
-    <div className='container items-center'>
-      <h2>Subscription Checkout</h2>
-      <button className='bg-green-400 rounded-xl p-2' onClick={() => handleSubscription('price_1OYPLvSAHpY80JOT0sx7MTtn')}>
+    <div className='flex bg-slate-200 w-96 rounded-xl container flex-col  items-center justify-center'>
+      <h2 classname='flex font-bold my-4 text-2xl text-slate-400 justify-center items-center'>Subscription Checkout</h2>
+      <div className='w-72 h-60 '>
+        <CardElement className='w-60 h-32 flex-col' />
+        <button className='bg-green-400 rounded-xl p-2' onClick={() => handleSubscription('price_1OYPLvSAHpY80JOT0sx7MTtn')}>
         Subscribe
       </button>
-      <div>
-        <CardElement />
       </div>
     </div>
   );
